801463f58ccb091437c550912c3eab93bb5a13b1
dhruvdcoder <dhruvdplayer@gmail.com>
2018-10-07 16:43:33 +0530

    Mathjax scroll

---
+++ static/js/mathjax/config.js
@@ -0,0 +1,18 @@
+MathJax.Hub.Config({
+
+      styles: {
+
+  ".MathJax_Display": {
+    display:"flex",
+    padding:"0.5em",
+    "overflow-x":"auto",
+    "box-sizing":"border-box"
+},
+".MathJax_Display .MathJax": {
+    display:"inline-block",
+    width:"100%"
+}
+
+}
+}
+);
